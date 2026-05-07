48b441af4220e78ae655024f4fa99eacb7dac3fb
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-07 16:12:43 -0600

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
+    alias(libs.plugins.android.lint) apply false
+    alias(libs.plugins.android.multiplatform.library) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.serialization) apply false
 }
