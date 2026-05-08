8b2663d42803026762a7b7b8ba269390728491cf
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-07 20:10:36 -0600

    Add

---
+++ library/src/androidMain/kotlin/com/lagradost/nicehttp-kmp/NiceResponseCompat.kt
@@ -0,0 +1,16 @@
+package com.lagradost.nicehttp
+
+import io.ktor.client.engine.okhttp.*
+
+/**
+ * Returns the underlying OkHttp Response if the engine is OkHttp, otherwise throws.
+ * Used by WebViewResolver which needs to inspect raw OkHttp response internals.
+ */
+val NiceResponse.okhttpResponse: okhttp3.Response
+    get() {
+        val call = response.call
+        return (call as? OkHttpCall)?.response
+            ?: throw UnsupportedOperationException(
+                "okhttpResponse is only available when using the OkHttp engine"
+            )
+    }
