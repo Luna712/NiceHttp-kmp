814533cb33a7ba61179d9c22312fe266172e5a54
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-09 15:05:55 -0600

    Try

--- library/src/appleMain/kotlin/com/lagradost/nicehttp-kmp/DefaultHttpClient.kt
+++ library/src/appleMain/kotlin/com/lagradost/nicehttp-kmp/DefaultHttpClient.kt
@@ -4,10 +4,8 @@ import io.ktor.client.*
 import io.ktor.client.engine.darwin.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.cache.*
-import platform.Foundation.NSURLCredential
-import platform.Foundation.NSURLSessionAuthChallengeUseCredential
-import platform.Foundation.credentialForTrust
-import platform.Security.SecTrustRef
+import platform.Foundation.*
+import platform.darwin.*
 
 actual fun defaultHttpClient(): HttpClient = HttpClient(Darwin) {
     install(HttpTimeout)
@@ -20,12 +18,26 @@ actual fun insecureHttpClient(): HttpClient = HttpClient(Darwin) {
     install(HttpCache)
     install(HttpRequestRetry) { noRetry() }
     engine {
-        handleChallenge { _, _, challenge, completionHandler ->
-            val trust = challenge.protectionSpace.serverTrust
-            completionHandler(
-                NSURLSessionAuthChallengeUseCredential,
-                NSURLCredential.credentialForTrust(trust)
-            )
+        configureSession {
+            setSessionDelegate(object : NSURLSessionDelegate(
+                NSObject(),
+                NSURLSessionTaskDelegateProtocol
+            ) {
+                override fun URLSession(
+                    session: NSURLSession,
+                    task: NSURLSessionTask,
+                    didReceiveChallenge: NSURLAuthenticationChallenge,
+                    completionHandler: (NSURLSessionAuthChallengeDisposition, NSURLCredential?) -> Unit
+                ) {
+                    // Accept all certificates
+                    completionHandler(
+                        NSURLSessionAuthChallengeUseCredential,
+                        NSURLCredential.credentialForTrust(
+                            didReceiveChallenge.protectionSpace.serverTrust!!
+                        )
+                    )
+                }
+            })
         }
     }
 }
