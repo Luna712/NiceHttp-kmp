1e19878e6a36a7286ca18bb1309fda90d84171fb
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-09 15:44:36 -0600

    Fix

--- library/src/mingwMain/kotlin/com/lagradost/nicehttp-kmp/DefaultHttpClient.kt
+++ library/src/mingwMain/kotlin/com/lagradost/nicehttp-kmp/DefaultHttpClient.kt
@@ -4,6 +4,7 @@ import io.ktor.client.*
 import io.ktor.client.engine.winhttp.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.cache.*
+import platform.winhttp.*
 
 actual fun defaultHttpClient(): HttpClient = HttpClient(WinHttp) {
     install(HttpTimeout)
@@ -16,6 +17,13 @@ actual fun insecureHttpClient(): HttpClient = HttpClient(WinHttp) {
     install(HttpCache)
     install(HttpRequestRetry) { noRetry() }
     engine {
-        sslVerification = SslVerification.NONE
+        protocolConfig {
+            securityFlags = (
+                SECURITY_FLAG_IGNORE_CERT_CN_INVALID or
+                SECURITY_FLAG_IGNORE_CERT_DATE_INVALID or
+                SECURITY_FLAG_IGNORE_UNKNOWN_CA or
+                SECURITY_FLAG_IGNORE_REVOCATION
+            )
+        }
     }
 }
