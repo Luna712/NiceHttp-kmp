b4cdaa9dfaf0a7ac81decc34b2633bee9b23c717
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-09 14:21:26 -0600

    Update

--- library/src/appleMain/kotlin/com/lagradost/nicehttp-kmp/DefaultHttpClient.kt
+++ library/src/appleMain/kotlin/com/lagradost/nicehttp-kmp/DefaultHttpClient.kt
@@ -1,6 +1,26 @@
-package com.lagradost.nicehttp.kmp
+package com.lagradost.nicehttp
 
 import io.ktor.client.*
 import io.ktor.client.engine.darwin.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.cache.*
 
-actual fun defaultHttpClient(): HttpClient = HttpClient(Darwin)
+actual fun defaultHttpClient(): HttpClient = HttpClient(Darwin) {
+    install(HttpTimeout)
+    install(HttpCache)
+    install(HttpRequestRetry) { noRetry() }
+}
+
+actual fun insecureHttpClient(): HttpClient = HttpClient(Darwin) {
+    install(HttpTimeout)
+    install(HttpCache)
+    install(HttpRequestRetry) { noRetry() }
+    engine {
+        handleChallenge { session, task, challenge, completionHandler ->
+            completionHandler(
+                platform.Foundation.NSURLSessionAuthChallengeDisposition.NSURLSessionAuthChallengeUseCredential,
+                platform.Foundation.NSURLCredential.credentialForTrust(challenge.protectionSpace.serverTrust)
+            )
+        }
+    }
+}
