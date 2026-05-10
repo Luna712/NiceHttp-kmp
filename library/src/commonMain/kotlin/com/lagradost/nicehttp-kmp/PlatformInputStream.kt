546b297a75e9e9bbaf2bfcdf42e5ff861121b55d
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-10 14:17:27 -0600

    Fix

--- library/src/commonMain/kotlin/com/lagradost/nicehttp-kmp/PlatformInputStream.kt
+++ library/src/commonMain/kotlin/com/lagradost/nicehttp-kmp/PlatformInputStream.kt
@@ -1,9 +1,9 @@
 package com.lagradost.nicehttp.kmp
 
 expect abstract class PlatformInputStream {
-    fun read(): Int
-    fun read(buffer: ByteArray, offset: Int, length: Int): Int
-    fun close()
+    abstract fun read(): Int
+    abstract fun read(buffer: ByteArray, offset: Int, length: Int): Int
+    abstract fun close()
 }
 
 internal expect fun ByteArray.toPlatformInputStream(): PlatformInputStream
