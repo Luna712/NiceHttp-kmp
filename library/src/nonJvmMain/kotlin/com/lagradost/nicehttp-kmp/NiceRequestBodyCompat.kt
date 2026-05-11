bbe4d99727ae859b12674d4c93bf695004770709
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-10 20:13:54 -0600

    -

--- library/src/nonJvmMain/kotlin/com/lagradost/nicehttp-kmp/NiceRequestBodyCompat.kt
+++ library/src/nonJvmMain/kotlin/com/lagradost/nicehttp-kmp/NiceRequestBodyCompat.kt
@@ -1,6 +1,6 @@
 package com.lagradost.nicehttp
 
-actual abstract class NiceRequestBodyCompat() actual constructor()
+actual abstract class NiceRequestBodyCompat
 internal actual fun NiceRequestBodyCompat.toRequestBody(): RequestBody =
     this as? RequestBody ?: throw UnsupportedOperationException(
         "NiceRequestBodyCompat must be a RequestBody on non-JVM platforms"
