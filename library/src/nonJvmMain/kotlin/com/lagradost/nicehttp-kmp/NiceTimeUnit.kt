4ef4e485255456f46560b124b47c34362d9040fa
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-10 21:01:05 -0600

    Update

--- library/src/nonJvmMain/kotlin/com/lagradost/nicehttp-kmp/NiceTimeUnit.kt
+++ library/src/nonJvmMain/kotlin/com/lagradost/nicehttp-kmp/NiceTimeUnit.kt
@@ -1,15 +1,4 @@
 package com.lagradost.nicehttp
 
-actual enum class NiceTimeUnit {
-    NANOSECONDS, MICROSECONDS, MILLISECONDS, SECONDS, MINUTES, HOURS, DAYS
-}
-
-internal actual fun NiceTimeUnit.toDurationUnit(): kotlin.time.DurationUnit = when (this) {
-    NiceTimeUnit.NANOSECONDS  -> kotlin.time.DurationUnit.NANOSECONDS
-    NiceTimeUnit.MICROSECONDS -> kotlin.time.DurationUnit.MICROSECONDS
-    NiceTimeUnit.MILLISECONDS -> kotlin.time.DurationUnit.MILLISECONDS
-    NiceTimeUnit.SECONDS      -> kotlin.time.DurationUnit.SECONDS
-    NiceTimeUnit.MINUTES      -> kotlin.time.DurationUnit.MINUTES
-    NiceTimeUnit.HOURS        -> kotlin.time.DurationUnit.HOURS
-    NiceTimeUnit.DAYS         -> kotlin.time.DurationUnit.DAYS
-}
+actual typealias NiceTimeUnit = kotlin.time.DurationUnit
+internal actual fun NiceTimeUnit.toDurationUnit(): kotlin.time.DurationUnit = this
