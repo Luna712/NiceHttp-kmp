b942b8aeb839e7c4d98f07666151189ff906f6a2
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-10 21:01:53 -0600

    Update

--- library/src/nonJvmMain/kotlin/com/lagradost/nicehttp-kmp/NiceInterceptorCompat.kt
+++ library/src/nonJvmMain/kotlin/com/lagradost/nicehttp-kmp/NiceInterceptorCompat.kt
@@ -1,8 +1,4 @@
 package com.lagradost.nicehttp
 
-actual interface NiceInterceptorCompat
-internal actual fun NiceInterceptorCompat.toInterceptor(): Interceptor =
-    this as? Interceptor ?: throw UnsupportedOperationException(
-        "NiceInterceptorCompat must be an Interceptor on non-JVM platforms"
-    )
-
+actual typealias NiceInterceptorCompat = Interceptor
+internal actual fun NiceInterceptorCompat.toInterceptor(): Interceptor = this
