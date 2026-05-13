6d351c53afc263cfd042077944e36fe6f3b147ea
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-13 13:57:55 -0600

    -

--- library/src/jvmCommonMain/kotlin/com/lagradost/nicehttp-kmp/OkHttpCompat.kt
+++ library/src/jvmCommonMain/kotlin/com/lagradost/nicehttp-kmp/OkHttpCompat.kt
@@ -101,7 +101,7 @@ private class ContinuationCallback(
 
     @OptIn(ExperimentalCoroutinesApi::class)
     override fun onResponse(call: Call, response: Response) {
-        continuation.resume(response) { _, _, _ -> call.cancel() }
+        continuation.resume(response) { _, _, _ -> }
     }
 
     override fun onFailure(call: Call, e: IOException) {
