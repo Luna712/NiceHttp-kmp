c5728ba1063190df922bee325468164bd68d9544
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-16 19:01:13 -0700

    Update

--- library/src/jvmCommonMain/kotlin/com/lagradost/nicehttp-kmp/NiceResponseCompat.kt
+++ library/src/jvmCommonMain/kotlin/com/lagradost/nicehttp-kmp/NiceResponseCompat.kt
@@ -1,3 +1,7 @@
 package com.lagradost.nicehttp
 
 actual typealias NiceResponseCompat = okhttp3.Response
+internal actual fun NiceResponse.resolveOkHttpResponseCompat(): NiceResponseCompat? {
+    val id = response.headers[NICEHTTP_OKHTTP_ID_HEADER] ?: return null
+    return OkHttpResponseCache.take(id)
+}
