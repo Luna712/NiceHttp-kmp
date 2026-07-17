c5d9423609f8fd729752826702f70229e61aa24a
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-16 20:45:28 -0700

    Internal

--- library/src/nonJvmMain/kotlin/com/lagradost/nicehttp-kmp/NiceResponseCompat.kt
+++ library/src/nonJvmMain/kotlin/com/lagradost/nicehttp-kmp/NiceResponseCompat.kt
@@ -1,4 +1,4 @@
 package com.lagradost.nicehttp
 
-actual typealias NiceResponseCompat = NiceResponse
+internal actual typealias NiceResponseCompat = NiceResponse
 internal actual fun NiceResponse.resolveOkHttpResponseCompat(): NiceResponseCompat? = this
