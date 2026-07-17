aced14f24fd2c92555e22b45a1b55b5ba94e5618
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-16 19:02:49 -0700

    Update

---
+++ library/src/jvmCommonMain/kotlin/com/lagradost/nicehttp-kmp/OkHttpResponseCache.kt
@@ -0,0 +1,41 @@
+package com.lagradost.nicehttp
+
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import java.util.Collections
+import java.util.UUID
+
+internal const val NICEHTTP_OKHTTP_ID_HEADER = "X-NiceHttp-OkHttp-Response-Id"
+
+internal object OkHttpResponseCache {
+    private const val maxEntries = 200
+
+    private val map = Collections.synchronizedMap(
+        object : LinkedHashMap<String, Response>(16, 0.75f, false) {
+            override fun removeEldestEntry(eldest: MutableMap.MutableEntry<String, Response>?): Boolean =
+                size > maxEntries
+        }
+    )
+
+    fun put(id: String, response: Response) {
+        map[id] = response
+    }
+
+    fun take(id: String): Response? = map.remove(id)
+}
+
+object OkHttpResponseCaptureInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val id = UUID.randomUUID().toString()
+        val response = chain.proceed(chain.request()).newBuilder()
+            .header(NICEHTTP_OKHTTP_ID_HEADER, id)
+            .build()
+        OkHttpResponseCache.put(id, response)
+        return response
+    }
+}
+
+fun OkHttpClient.Builder.addNiceHttpResponseCapture(): OkHttpClient.Builder =
+    addInterceptor(OkHttpResponseCaptureInterceptor)
+
