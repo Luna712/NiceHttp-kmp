0baf0ded27ebfda7a2983c28014c8fd4dda840a1
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-18 13:09:39 -0600

    -

--- library/src/jsMain/kotlin/com/lagradost/nicehttp-kmp/RunBlocking.kt
+++
@@ -1,4 +0,0 @@
-package com.lagradost.nicehttp
-
-internal actual fun <T> runBlockingCompat(block: suspend () -> T): T =
-    throw UnsupportedOperationException("Use suspend text() on JS. runBlocking is not supported.")
