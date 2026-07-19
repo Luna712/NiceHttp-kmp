7f51a9b6e883924bf05fffd138e3343b370d64eb
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-19 11:47:25 -0600

    Add

---
+++ library/src/nonJvmMain/kotlin/com/lagradost/nicehttp-kmp/NiceHeadersCompat.kt
@@ -0,0 +1,5 @@
+package com.lagradost.nicehttp
+
+actual typealias NiceHeadersCompat = NiceHeaders
+internal actual fun NiceResponse.resolveOkHttpHeadersCompat(): NiceHeadersCompat? = responseHeaders
+
