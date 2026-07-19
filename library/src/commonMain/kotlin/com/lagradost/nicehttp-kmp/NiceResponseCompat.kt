56514897a594f0c1c5911e4759a8a2513971e9b8
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-19 14:48:02 -0600

    Non-nullable

--- library/src/commonMain/kotlin/com/lagradost/nicehttp-kmp/NiceResponseCompat.kt
+++ library/src/commonMain/kotlin/com/lagradost/nicehttp-kmp/NiceResponseCompat.kt
@@ -1,4 +1,4 @@
 package com.lagradost.nicehttp
 
 expect class NiceResponseCompat
-internal expect fun NiceResponse.resolveOkHttpResponseCompat(): NiceResponseCompat?
+internal expect fun NiceResponse.resolveOkHttpResponseCompat(): NiceResponseCompat
