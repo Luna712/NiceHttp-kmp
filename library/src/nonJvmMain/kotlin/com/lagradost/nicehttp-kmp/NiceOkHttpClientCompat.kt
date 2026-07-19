d698553890c0a49ef67e50ca977dace020a8885e
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-19 13:36:18 -0600

    Add NiceOkHttpClientCompat

---
+++ library/src/nonJvmMain/kotlin/com/lagradost/nicehttp-kmp/NiceOkHttpClientCompat.kt
@@ -0,0 +1,6 @@
+package com.lagradost.nicehttp
+
+import io.ktor.client.HttpClient
+
+actual class NiceOkHttpClientCompat internal constructor(internal val client: HttpClient)
+internal actual fun NiceOkHttpClientCompat.toHttpClient(): HttpClient = client
