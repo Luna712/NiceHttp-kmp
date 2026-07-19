690f5fb4fd70ca670da7077a52623ace60c9ba2d
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-19 17:19:21 -0600

    Add

--- library/src/commonMain/kotlin/com/lagradost/nicehttp-kmp/NiceOkHttpClientCompat.kt
+++ library/src/commonMain/kotlin/com/lagradost/nicehttp-kmp/NiceOkHttpClientCompat.kt
@@ -2,3 +2,4 @@ package com.lagradost.nicehttp
 
 expect class NiceOkHttpClientCompat
 internal expect fun NiceOkHttpClientCompat.toHttpClient(): io.ktor.client.HttpClient
+internal expect fun defaultNiceOkHttpClientCompat(): NiceOkHttpClientCompat
