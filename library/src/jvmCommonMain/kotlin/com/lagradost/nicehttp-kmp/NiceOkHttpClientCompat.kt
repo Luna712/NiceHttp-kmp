f6a004351acbe62cbe30a277e7807e6669635455
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-19 13:35:16 -0600

    Add NiceOkHttpClientCompat

---
+++ library/src/jvmCommonMain/kotlin/com/lagradost/nicehttp-kmp/NiceOkHttpClientCompat.kt
@@ -0,0 +1,7 @@
+package com.lagradost.nicehttp
+
+import io.ktor.client.HttpClient
+import okhttp3.OkHttpClient
+
+actual typealias NiceOkHttpClientCompat = OkHttpClient
+internal actual fun NiceOkHttpClientCompat.toHttpClient(): HttpClient = defaultHttpClient(this)
