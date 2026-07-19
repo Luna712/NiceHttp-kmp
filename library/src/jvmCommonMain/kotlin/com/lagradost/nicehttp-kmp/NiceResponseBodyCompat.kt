cfcade70e3e4ed2e118c280a9b1b191a620258dd
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-19 14:47:08 -0600

    -toResponseBody

--- library/src/jvmCommonMain/kotlin/com/lagradost/nicehttp-kmp/NiceResponseBodyCompat.kt
+++ library/src/jvmCommonMain/kotlin/com/lagradost/nicehttp-kmp/NiceResponseBodyCompat.kt
@@ -1,9 +1,7 @@
 package com.lagradost.nicehttp
 
-import okhttp3.ResponseBody.Companion.toResponseBody
-
 actual typealias NiceResponseBodyCompat = okhttp3.ResponseBody
 internal actual fun NiceResponse.resolveOkHttpResponseBodyCompat(): NiceResponseBodyCompat {
     @Suppress("DEPRECATION")
-    return okhttpResponse?.body ?: "".toResponseBody()
+    return okhttpResponse.body
 }
