ce7aa092dcf2210e297148f8ccbaee53b9021fbd
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-19 14:48:45 -0600

    Non-nullable

--- library/src/commonMain/kotlin/com/lagradost/nicehttp-kmp/NiceResponse.kt
+++ library/src/commonMain/kotlin/com/lagradost/nicehttp-kmp/NiceResponse.kt
@@ -94,7 +94,7 @@ class NiceResponse(
         ReplaceWith("this"),
         DeprecationLevel.WARNING,
     )
-    val okhttpResponse: NiceResponseCompat? get() = resolveOkHttpResponseCompat()
+    val okhttpResponse: NiceResponseCompat get() = resolveOkHttpResponseCompat()
 
     @Deprecated(
         "Use suspend fun body() instead.",
