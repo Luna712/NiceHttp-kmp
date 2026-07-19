8fd315f44d0f116f17bc9960e3014d0e499cd53d
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-19 12:20:12 -0600

    Try fix

--- library/src/nonJvmMain/kotlin/com/lagradost/nicehttp-kmp/NiceResponseBodyCompat.kt
+++ library/src/nonJvmMain/kotlin/com/lagradost/nicehttp-kmp/NiceResponseBodyCompat.kt
@@ -1,4 +1,4 @@
 package com.lagradost.nicehttp
 
-actual typealias NiceResponseBodyCompat = ResponseBody
-internal actual fun NiceResponse.resolveOkHttpResponseBodyCompat(): NiceResponseBodyCompat? = runBlockingCompat { body() }
+actual typealias NiceResponseBodyCompat = io.ktor.client.statement.HttpResponse
+internal actual fun NiceResponse.resolveOkHttpResponseBodyCompat(): NiceResponseBodyCompat? = response
