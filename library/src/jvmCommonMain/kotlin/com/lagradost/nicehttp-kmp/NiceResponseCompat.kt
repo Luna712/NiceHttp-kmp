7e2870d763f97bbd5a75ff8a734c3f8c7e0e62e6
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-19 14:52:42 -0600

    Add comment

--- library/src/jvmCommonMain/kotlin/com/lagradost/nicehttp-kmp/NiceResponseCompat.kt
+++ library/src/jvmCommonMain/kotlin/com/lagradost/nicehttp-kmp/NiceResponseCompat.kt
@@ -2,6 +2,7 @@ package com.lagradost.nicehttp
 
 actual typealias NiceResponseCompat = okhttp3.Response
 internal actual fun NiceResponse.resolveOkHttpResponseCompat(): NiceResponseCompat {
+    // Exception should never really be given but included for source compatibility to make okhttpResponse non-nullable.
     val missingResponseError = "Missing okhttp response. Try removing okhttpResponse and using NiceResponse directly."
     val id = response.headers[NICEHTTP_OKHTTP_ID_HEADER] ?: throw IllegalStateException(missingResponseError)
     return OkHttpResponseCache.take(id) ?: throw IllegalStateException(missingResponseError)
