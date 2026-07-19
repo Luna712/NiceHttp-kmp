4de19700dbb7dc44c6ffc6030e7067d9200177c6
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-19 14:46:37 -0600

    -headersof

--- library/src/jvmCommonMain/kotlin/com/lagradost/nicehttp-kmp/NiceHeadersCompat.kt
+++ library/src/jvmCommonMain/kotlin/com/lagradost/nicehttp-kmp/NiceHeadersCompat.kt
@@ -1,9 +1,7 @@
 package com.lagradost.nicehttp
 
-import okhttp3.Headers.Companion.headersOf
-
 actual typealias NiceHeadersCompat = okhttp3.Headers
 internal actual fun NiceResponse.resolveOkHttpHeadersCompat(): NiceHeadersCompat {
     @Suppress("DEPRECATION")
-    return okhttpResponse?.headers ?: headersOf()
+    return okhttpResponse.headers
 }
